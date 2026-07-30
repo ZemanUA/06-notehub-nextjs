1ca41260310e94f65ff84a2ef15baf26c1b49209
Valentyn Ziomenko <ziomenkow@gmail.com>
2026-07-30 16:18:46 +0200

    "rework TanStackProvider as in documantation"

--- app/notes/[noteId]/page.tsx
+++ app/notes/[noteId]/page.tsx
@@ -8,18 +8,18 @@ interface NoteDetailsProps{
 
 export default async function NoteDetails (props : NoteDetailsProps){
     const {noteId} = await props.params;
-    const note = fetchNoteById(noteId);
-    const date = new Date((await note).createdAt).toLocaleDateString();
+    const note = await fetchNoteById(noteId);
+    const date = new Date(note.createdAt).toLocaleDateString();
 
     return(
         <main className={css.main}>	
 	<div className={css.container}>
 		<div className={css.item}>
 		  <div className={css.header}>
-		    <h2>{(await note).title}</h2>
+		    <h2>{note.title}</h2>
 		  </div>
-		  <p className={css.tag}>{(await note).tag}</p>
-		  <p className={css.content}>{(await note).content}</p>
+		  <p className={css.tag}>{note.tag}</p>
+		  <p className={css.content}>{note.content}</p>
 		  <p className={css.date}>{date}</p>
 		</div>
 	</div>
--- components/TanStackProvider/TanStackProvider.tsx
+++ components/TanStackProvider/TanStackProvider.tsx
@@ -1,17 +1,36 @@
-"use client"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+'use client'
+import {
+  environmentManager,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 
-const queryClient = new QueryClient();
-
-interface TanStackProviderProps {
-    children: React.ReactNode;
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+    },
+  })
 }
 
-export default function TanStackProvider({children} : TanStackProviderProps){
-    return (
-        <QueryClientProvider client={queryClient}>
-            {children}
-        <ReactQueryDevtools initialIsOpen={false}/>
-        </QueryClientProvider>)
+let browserQueryClient: QueryClient | undefined = undefined
+
+function getQueryClient() {
+  if (environmentManager.isServer()) {
+    return makeQueryClient()
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeQueryClient()
+    return browserQueryClient
+  }
+}
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+
+  const queryClient = getQueryClient()
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  )
 }
